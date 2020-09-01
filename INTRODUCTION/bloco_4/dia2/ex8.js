b59934a8f1d3324de12aff1d1c5d73cdc04967b4
spawnred <bruno.spawnhell@gmail.com>
2020-08-31 23:36:42 -0300

    exercicios do bloco 4 aula 2

---
+++ INTRODUCTION/bloco_4/dia2/ex8.js
@@ -0,0 +1,6 @@
+let num = [];
+
+for(let i = 1; i < 26; i++ ){
+    num.push(i);
+}
+console.log(num);
---
+++ INTRODUCTION/bloco_4/dia2/ex9.js
@@ -0,0 +1,10 @@
+let num = [];
+let div = 0;
+
+for(let i = 1; i < 26; i++ ){
+    num.push(i);
+}
+for(let j = 0; j < num.length; j++){
+    div = num[j]/2;
+    console.log(div);
+}
