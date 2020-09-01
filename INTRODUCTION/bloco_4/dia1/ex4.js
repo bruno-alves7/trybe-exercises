167682ae3be6a9e81aef7adf94cfd5c6d09e4ba2
spawnred <bruno.spawnhell@gmail.com>
2020-08-31 21:57:37 -0300

    Incluindo exercicios bloco 4 dia 1

---
+++ INTRODUCTION/bloco_4/dia1/ex4.js
@@ -0,0 +1,9 @@
+let a = -45
+
+if(a>0){
+    console.log(`${a} é positivo`)
+} else if (a<0){
+    console.log(`${a} é negativo`)
+} else {
+    console.log(`${a} é zero`)
+}
---
+++ INTRODUCTION/bloco_4/dia1/ex5.js
@@ -0,0 +1,11 @@
+let a = 90;
+let b = 45;
+let c = 48;
+let ver = false;
+
+if(a+b+c==180){
+    ver = true;
+    console.log(`${ver}, os ângulos ${a}, ${b} e ${c} formam um triângulo`)
+} else {
+    console.log(`${ver}, os ângulos ${a}, ${b} e ${c} não formam um triângulo`)
+}
